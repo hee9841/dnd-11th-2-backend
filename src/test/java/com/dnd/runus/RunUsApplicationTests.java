e5aa8358a39ff4ed1fd044b00da9954e1f299e7f
Jaewon Lee <jaewonlee.pro@gmail.com>
2024-07-17 20:38:24 +0900

    Feat: init springboot project

---
+++ src/test/java/com/dnd/runus/RunUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.runus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunUsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
