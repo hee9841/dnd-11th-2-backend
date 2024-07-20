c068a7d7b5890b86fd30e081c81022e8f718f329
이재원 <jaewonlee.pro@gmail.com>
2024-07-20 22:33:28 +0900

    Chore: spotless 플러그인 추가 (#6)
    * Chore: spotless 플러그인 추가
    
    * Chore: pre-commit 스크립트 추가
    
    * Style: 기존 코드에 spotless 코드 스타일 적용
    
    * Chore: spotless test 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.5"
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "com.dnd"
@@ -29,4 +30,55 @@ tasks.withType<Test> {
 
 tasks.bootBuildImage {
     createdDate = "now"
+}
+
+spotless {
+    encoding = Charsets.UTF_8
+
+    java {
+        target("**/*.java")
+        targetExclude("build/**", "**/*Request*.java", "**/*Response*.java", "**/*Dto*.java")
+        palantirJavaFormat("2.47.0")
+    }
+
+    java {
+        target("**/*.java")
+        endWithNewline()
+        indentWithSpaces(4)
+        trimTrailingWhitespace()
+        importOrder("", "java|javax", "\\#").wildcardsLast()
+        removeUnusedImports()
+        formatAnnotations()
+    }
+
+    format("misc") {
+        // define the files to apply `misc` to
+        target("*.gradle", "*.gradle.*", ".gitattributes", ".gitignore")
+
+        // define the steps to apply to those files
+        endWithNewline()
+        indentWithSpaces(4)
+        trimTrailingWhitespace()
+    }
+}
+
+tasks.register<Copy>("updateGitHooks") {
+    from("scripts/pre-commit.sh")
+    into(".git/hooks")
+    rename("pre-commit.sh", "pre-commit")
+}
+
+tasks.register<Exec>("makeGitHooksExecutable") {
+    commandLine("chmod", "+x", ".git/hooks/pre-commit")
+    dependsOn("updateGitHooks")
+}
+
+tasks.compileJava {
+    dependsOn("makeGitHooksExecutable")
+}
+
+tasks.register("projectTest") {
+    dependsOn("spotlessJavaCheck")
+    dependsOn("compileJava")
+    dependsOn("test")
 }
