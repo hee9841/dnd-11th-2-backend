3e435614f380a2c02c945c2673970ec137926968
이재원 <jaewonlee.pro@gmail.com>
2024-08-04 14:56:22 +0900

    Feat: member id resolver 추가 (#33)

--- src/main/java/com/dnd/runus/presentation/config/WebConfig.java
+++ src/main/java/com/dnd/runus/presentation/config/WebConfig.java
@@ -1,10 +1,14 @@
 package com.dnd.runus.presentation.config;
 
 import com.dnd.runus.presentation.handler.NullResponseHandler;
+import com.dnd.runus.presentation.resolver.MemberIdResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -14,5 +18,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(nullResponseHandler);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new MemberIdResolver());
     }
 }
