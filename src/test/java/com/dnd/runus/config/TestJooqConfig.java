7bd19a0519437f3346b7bc120108a3f069404a25
이재원 <jaewonlee.pro@gmail.com>
2024-09-06 21:37:21 +0900

    Chore: 테스트 환경에서 jOOQ logging 설정 (#146)

--- src/test/java/com/dnd/runus/config/TestJooqConfig.java
+++ src/test/java/com/dnd/runus/config/TestJooqConfig.java
@@ -1,9 +1,15 @@
 package com.dnd.runus.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jooq.Configuration;
 import org.jooq.DSLContext;
+import org.jooq.ExecuteContext;
+import org.jooq.ExecuteListener;
 import org.jooq.impl.DSL;
 import org.jooq.impl.DataSourceConnectionProvider;
+import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.springframework.boot.autoconfigure.jooq.JooqAutoConfiguration;
 import org.springframework.boot.autoconfigure.jooq.JooqProperties;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -17,11 +23,17 @@ import javax.sql.DataSource;
     JooqAutoConfiguration.class,
     JooqProperties.class,
 })
+@Slf4j
 @TestConfiguration
 @RequiredArgsConstructor
 public class TestJooqConfig {
     private final DataSource dataSource;
     private final JooqProperties jooqProperties;
+
+    static {
+        System.setProperty("org.jooq.no-logo", "true");
+        System.setProperty("org.jooq.no-tips", "true");
+    }
 
     @Bean
     DataSourceConnectionProvider connectionProvider() {
@@ -30,6 +42,17 @@ public class TestJooqConfig {
 
     @Bean
     DSLContext dsl() {
-        return DSL.using(connectionProvider(), jooqProperties.determineSqlDialect(dataSource));
+        Configuration configuration = new DefaultConfiguration()
+                .set(connectionProvider())
+                .set(jooqProperties.determineSqlDialect(dataSource))
+                .set(new DefaultExecuteListenerProvider(new LoggingExecuteListener())); // For logging SQL
+        return DSL.using(configuration);
+    }
+
+    public static class LoggingExecuteListener implements ExecuteListener {
+        @Override
+        public void executeStart(ExecuteContext ctx) {
+            log.info("jOOQ: {}", ctx.query());
+        }
     }
 }
