a36b804f036a0fada75567116d710244bdb13585
sungHeeLee <70899677+hee9841@users.noreply.github.com>
2024-09-09 20:57:20 +0900

    Fix : 컴파일 시 발생하는 unknown enum constant When.MAYBE 경고 제거 (#165)
    * Fix : 컴파일 시 발생하는 unknown enum constant When.MAYBE 경고 제거
    
    * Fix : 최신 버전으로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,9 @@ dependencies {
 
     implementation(libs.bcpkix)
 
+    //defect detection tool for Java that uses static analysis to look for more than 200 bug patterns
+    implementation(libs.spotbugs)
+
     // JWT
     implementation(libs.jjwt.api)
     runtimeOnly(libs.jjwt.impl)
